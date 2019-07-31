d48740f0e80a0ce74027de2bbc187b95fe8734d4
grace-mugoiri <gracemugoiri@gmail.com>
2019-07-31 16:44:40 +0300

    add the post models and user migrations for user_id

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = current_user.posts.build(posts_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render 'posts/new'
+        end
+    end
+
+    private
+
+    def posts_params
+        params.require(:post).permit(:body)
+    end
+end
