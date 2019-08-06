768081c35ebf8bf37522562125b8073e975dbfcc
grace-mugoiri <gracemugoiri@gmail.com>
2019-08-06 19:03:05 +0300

    added model and controller for friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -38,5 +40,29 @@ class User < ApplicationRecord
       user.bio="Some Bio"
       user.address = {country:"Some", city:"city", street:"street", zipcode:"123"}
     end
+  end
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
   end
 end
