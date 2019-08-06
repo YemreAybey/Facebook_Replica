8a7db6688dfbf7c72e4ebe639d9b19c74ded8e0d
Yemre <aybey77@gmail.com>
2019-08-06 17:28:28 +0300

    like test refined

--- test/integration/like_post_test.rb
+++ test/integration/like_post_test.rb
@@ -4,6 +4,7 @@ class LikePostTest < ActionDispatch::IntegrationTest
     def setup
         @user = users(:Emre)
         @post = posts(:one)
+        @like = likes(:one)
     end
 
       test "user can like a post when logged in" do
@@ -19,28 +20,8 @@ class LikePostTest < ActionDispatch::IntegrationTest
         assert_difference '@post.likes.count', 1 do
         post likes_path, params: { like: { post_id: @post.id, user_id: @user.id } }
         end
-        assert_redirected_to posts_path
-    end
-
-    test "user can unlike a post" do
-        get user_session_path
-        assert_equal 200, status
-        post user_session_path, params: {user: {
-          email:    @user.email,
-          password: "password"
-        }}
-        follow_redirect!
-        assert_equal 200, status
-        get posts_path
-        assert_difference '@post.likes.count' do
-        post likes_path, params: { like: { post_id: @post.id, user_id: @user.id } }
+        assert_difference 'Like.count', -1 do
+          delete like_path(@like.id)
         end
-        assert_redirected_to posts_path
-        follow_redirect!
-        like = Like.last  
-        delete like_path(like.id)
-        follow_redirect!
-        assert_select 'input', 'Unlike'
     end
-
 end
